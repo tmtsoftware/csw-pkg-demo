5b1a6da1e59db1edc75fdc6621f3041c28ce6b62
abrighton <allanexus@gmail.com>
2016-05-16 22:08:16 +0200

    Just comments

--- hcd2/src/main/scala/csw/pkgDemo/hcd2/Hcd2.scala
+++ hcd2/src/main/scala/csw/pkgDemo/hcd2/Hcd2.scala
@@ -7,17 +7,7 @@ import csw.services.pkg.Supervisor._
 import csw.services.pkg.{Hcd, LifecycleHandler}
 import csw.util.cfg.Configurations.SetupConfig
 
-//// A test HCD that is configured with the given name and config path
-//object Hcd2 {
-//  /**
-//   * Used to create the actor
-//   */
-//  def props(info: HcdInfo): Props = Props(classOf[Hcd2], info)
-//}
-
-/**
- * Test HCD
- */
+// A test HCD that is configured with the given name and config path
 case class Hcd2(info: HcdInfo) extends Hcd with HcdController with LifecycleHandler {
   val worker = context.actorOf(Hcd2Worker.props(info.prefix))
 
