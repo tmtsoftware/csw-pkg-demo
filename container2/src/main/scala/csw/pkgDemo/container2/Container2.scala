f6796c3dbe6e52db9e53c8ea3fd5e717d07ee1e4
abrighton <allanexus@gmail.com>
2016-10-04 22:49:51 +0200

    Updated for changes to ContainerCmd

--- container2/src/main/scala/csw/pkgDemo/container2/Container2.scala
+++ container2/src/main/scala/csw/pkgDemo/container2/Container2.scala
@@ -6,6 +6,10 @@ import csw.services.apps.containerCmd.ContainerCmd
  * Creates container2 based on resources/container2.conf
  */
 object Container2 extends App {
-  private val a = args // Required to avoid null args below
-  ContainerCmd("container2", a, Some("container2.conf"))
+  // This defines the names that can be used with the --start option and the config files used ("" is the default entry)
+  val m = Map(
+    "" -> "container2.conf" // default value
+  )
+  // Parse command line args for the application (app name is container2, like the sbt project)
+  ContainerCmd("container2", args, m)
 }
